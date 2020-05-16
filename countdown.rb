c2be5f04020b0451c41319f61bf141e10687c126
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 05:05:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,14 @@
 #write your code here
-number = 10
-
-def countdown
-  while number <= 10 
-    puts "{number} SECOND(S)!"
-    number -= 1
-  end 
-puts "HAPPY NEW YEAR!"
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+    
+def countdown_with_sleep(number)
+    sleep 5
+end
 
 end
